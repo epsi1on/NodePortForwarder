bfec63ae93375b73baaf6ff5c678959e5beaf0ac
epsi1on <ehsan.ma@gmx.com>
2018-01-21 18:09:44 +0330

    update

--- bin/forwarder.js
+++ bin/forwarder.js
@@ -23,6 +23,38 @@ if(listenOn == null || forwardTo == null || action == null)
     console.log('   action: the action, three possible values: encrypt, decrypt, none');
     console.log('example: node forward.js -l "127.0.0.1:8080" -f "127.0.0.1:80" -a "encrypt"');
     console.log('above example will listen on 127.0.0.1:8080 and forwards connections into 127.0.0.1:80 and encrypt data');
+    console.log('hovewer the IP list of this host is:');
+    {
+        'use strict';
+
+        var os = require('os');
+        var ifaces = os.networkInterfaces();
+
+        Object.keys(ifaces).forEach(function (ifname) {
+            var alias = 0;
+
+            ifaces[ifname].forEach(function (iface) {
+                if ('IPv4' !== iface.family || iface.internal !== false) {
+                    // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                    //return;
+                    //console.log(iface);
+                }
+
+                if (alias >= 1) {
+                    // this single interface has multiple ipv4 addresses
+                    console.log(' - '+ifname + ':' + alias, iface.address);
+                } else {
+                    // this interface has only one ipv4 adress
+                    console.log(' - '+ifname, iface.address);
+                }
+                ++alias;
+            });
+        });
+
+    }
+
+    console.log();
+    //publicIp.isIPv4
 }
 else
 {
