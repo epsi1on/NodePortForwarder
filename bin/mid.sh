c3ef7e8e4bde8aab930dda77c0e447039070e1c1
epsi1on <ehsan.ma@gmx.com>
2018-11-27 22:09:23 +0330

    update

---
+++ bin/mid.sh
@@ -0,0 +1,13 @@
+local_ip="$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')"
+echo please enter the other side socket address
+read remote_socket
+
+cd /home && mkdir nodefw && cd nodefw
+
+rm -rf NodePortForwarder
+
+git clone https://github.com/epsi1on/NodePortForwarder.git
+
+local_socket = "$local_ip:8080"
+
+node NodePortForwarder/bin/forwarder.js -l "$local_socket" -f "$remote_socket" -a "none"
